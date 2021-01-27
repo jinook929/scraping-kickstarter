3d381321a5891b983f5d782187e3e8c6a6c2717a
jinook929 <jinook929@gmail.com>
2021-01-27 18:59:07 -0500

    commit tree 0368a7bcabdadd843b5f2e8e9a68a6eca5042c79

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,56 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text 
+  # location: project.css("ul.project-meta span.location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
 
 def create_project_hash
   # write your code here
+
+  html = File.read("fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html)
+  # binding.pry
+
+  Hash.new.tap {|projects|
+    kickstarter.css("li.project.grid_4").each {|project| 
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+          :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+          :description => project.css("p.bbcard_blurb").text,
+          :location => project.css("ul.project-meta span.location-name").text,
+          :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
+    }
+    # kickstarter.css("li.project.grid_4").each {|project|
+    #   title = project.css("h2 strong a").text.strip
+    #   projects[title.to_sym] = {
+    #     image_link: project.css(".project-thumnail a img").attribute("src").value,
+    #     description: project.css("p.bbcard_blurb").text.strip,
+    #     location: project.css(".project-meta span.location-name"),
+    #     percent_funded: project.css("project-stats li strong").text.strip.gsub("%", "").to_i
+    #   }
+    # }
+  }
+  
+
+  # projects = {}
+
+  # kickstarter.css("li.project.grid_4").each {|project| 
+  #   title = project.css("h2.bbcard_name strong a").text
+  #   projects[title.to_sym] = {
+  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  #     :description => project.css("p.bbcard_blurb").text,
+  #     :location => project.css("ul.project-meta span.location-name").text,
+  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  #   }
+  # }
+  # binding.pry
+  # p projects
 end
+
+create_project_hash
